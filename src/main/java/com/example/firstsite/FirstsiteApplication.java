19fb11b71cda6e5ed3302296163d014eb262c9cb
Dmitry <d.boshchenko@gmail.com>
2026-04-07 14:29:17 +0300

    inti

---
+++ src/main/java/com/example/firstsite/FirstsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstsite/FirstsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
